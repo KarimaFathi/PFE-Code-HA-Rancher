db7b6f87f5ec8b3d4fdf94add810b0af197f221e
Karima Fathi <karimafathifadili@gmail.com>
2023-08-15 23:34:04 +0000

    infrastructure provisioning with terraform

--- TF-Code/TF-code/main_project/main.tf
+++ TF-Code/TF-code/main_project/main.tf
@@ -21,23 +21,3 @@ module "vsphere_infra_rancher" {
   ipv4_gateway     = var.ipv4_gateway
 }
 
-module "kubeadm-token" {
-  source = "github.com/scholzj/terraform-kubeadm-token"
-}
-
-module "rke2_rancher" {
- source = "../modules/rke2_rancher_install_module"
-
-
-  vsphere_server   = var.vsphere_server
-  vsphere_user     = var.vsphere_user
-  vsphere_password = var.vsphere_password
-  ssh_user         = var.ssh_user
-  ipv4_address     = var.ipv4_address
-  ssh_key          = var.ssh_key
-  RKE2_token       = module.kubeadm-token.token
-  DNS_domain       = var.DNS_domain
-  vm_count         = var.vm_count
-  ssh_pass        = var.ssh_pass
-}
-
